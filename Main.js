69d78a914a960445bc6948e8ca6ebf95feaa5c12
EthanSinu <138210207+EthanSinu@users.noreply.github.com>
2023-11-03 13:15:01 +0400

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,8 @@
+var first_name = "Ranbir";
+var second_name = "Kapoor";
+var full_name = first_name.concat(second_name);
+console.log(full_name);
+var a = 10;
+var b = 5;
+console.log(a);
+console.log(b);
